bda2be8782c6f4fbf8629570606545518b43e42d
liuqinghui <liuqinghui@weimiao.cn>
2021-11-08 12:42:12 +0800

    update cron

--- initialize/cron.go
+++ initialize/cron.go
@@ -25,9 +25,9 @@ func addJob(c *cron.Cron) {
 	// 爬取每日基金排行榜单(每天 13:30)
 	_, _ = c.AddJob("0 00 17 */1 * *", crontab.FundTopCron{})
 	// 爬取基金基本信息(每天 1:30)
- 	_, _ = c.AddJob("0 00 19 */1 * *", crontab.FundBasicCron{})
-	// 爬取基金持仓信息信息(每天 20:30)
-	_, _ = c.AddJob("0 00 21 */1 * *", crontab.FundStockCron{})
+ 	_, _ = c.AddJob("@every 2h", crontab.FundBasicCron{})
+	// 爬取基金持仓信息信息(每小时一次 20:30)
+	_, _ = c.AddJob("@every 2h", crontab.FundStockCron{})
 
 }
 
